54fd89e11f87c2498e393d366d71d2266a50e24c
Eduardo Perez Salcedo <a.eduardoperez.fp2019@gmail.com>
2024-02-20 13:35:31 -0400

    feat(Tags): Agregamos documentacion con swagger en el endpoint de tags

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 // Services
 import { TagsService } from './tags.service';
@@ -11,7 +11,7 @@ import { Tags } from './entities/tags.entity';
 import { ROLES } from 'src/commons/models';
 
 // Dtos
-import { TagsCreatetDto } from './dto/createTags.dto';
+import { ResponseTagsCreateDto, TagsCreatetDto } from './dto/createTags.dto';
 
 // Auth
 import { Roles } from 'src/auth/decorators/roles.decorator';
@@ -23,6 +23,24 @@ export class TagsController {
   constructor(private readonly tagsServices: TagsService) {}
 
   @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Create Tag.',
+    description: 'this endpoint is for create a tag for post.',
+  })
+  @ApiBody({
+    type: TagsCreatetDto,
+    description: 'The fields to be created.',
+  })
+  @ApiResponse({
+    status: 201,
+    type: ResponseTagsCreateDto,
+    description: 'create tag successfully.',
+  })
+  @ApiResponse({
+    status: 409,
+    type: () => 'CONFLICT',
+    description: 'The tag already exists.',
+  })
   @UseGuards(JwtAuthGuard)
   @Roles(ROLES.SUPERADMIN, ROLES.ADMIN)
   @Post()
@@ -31,6 +49,14 @@ export class TagsController {
   }
 
   @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Get all Tags.',
+    description: 'this endpoint is for get all tags.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: [Tags],
+  })
   @UseGuards(JwtAuthGuard)
   @Roles(ROLES.SUPERADMIN, ROLES.ADMIN)
   @Get()
@@ -39,6 +65,20 @@ export class TagsController {
   }
 
   @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Get tag by id.',
+    description: 'this endpoint is for get tag by id.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: Tags,
+    description: 'response get tag by id.',
+  })
+  @ApiResponse({
+    status: 404,
+    type: () => 'NOT_FOUND',
+    description: 'The tag not found.',
+  })
   @UseGuards(JwtAuthGuard)
   @Roles(ROLES.SUPERADMIN, ROLES.ADMIN)
   @Get(':id')
